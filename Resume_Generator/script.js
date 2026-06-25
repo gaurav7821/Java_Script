d2304ea20e35d095569fad4006159d2ef7ec4648
chaudharigaurav865@gmail.com <chaudharigaurav865@gmail.com>
2026-06-25 15:41:01 +0530

    Resume Generator Project

---
+++ Resume_Generator/script.js
@@ -0,0 +1,14 @@
+function addNewWEField() {
+
+    let newNode = document.createElement("textArea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+
+    let weOb = document.getElementById("we");
+    let weAddButtonOb = document.getElementById("weAddButton");
+
+    weOb.insertBefore(newNode, weAddButtonOb);
+};
